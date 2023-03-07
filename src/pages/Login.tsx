7a1e4a8ceb71c79bfbdd11a336316ef59dd076e9
twothicc <chenenliuu@gmail.com>
2023-03-07 14:38:36 +0800

    Change to hide login signup buttons accordingly

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -22,13 +22,17 @@ const Login = () => {
 
   return (
       <div className="flex min-h-screen justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="absolute flex flex-col w-1/3 top-1/4">
+        <div className="absolute flex flex-col max-w-md w-1/3 top-1/4">
         <div className="flex flex-row justify-center mb-5">
           <button
             name={LoginSwap}
             type="button"
-            className={"group relative flex w-full justify-center rounded-md mr-1 bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"}
-            hidden={isLogin}
+            className={
+              !isLogin ? 
+              "group relative flex w-full justify-center rounded-md mr-1 bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              :
+              "group relative flex w-full justify-center rounded-md mr-1 bg-gray-300 py-2 px-3 text-sm font-semibold text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+            }
             onClick={handleSwap}
           >
               Login
@@ -36,8 +40,12 @@ const Login = () => {
           <button
             name={SignUpSwap}
             type="button"
-            className="group relative flex w-full justify-center rounded-md ml-1 bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            hidden={!isLogin}
+            className={
+              isLogin ? 
+              "group relative flex w-full justify-center rounded-md mr-1 bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              :
+              "group relative flex w-full justify-center rounded-md mr-1 bg-gray-300 py-2 px-3 text-sm font-semibold text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+            }
             onClick={handleSwap}
           >
               Sign Up
